0bf0f30ff2cfd8cae4721ba780671fc18a1a523f
akv0122 <124851153+akv0122@users.noreply.github.com>
2026-07-07 01:12:31 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length())
+            return false; 
+
+        char[] cs = s.toCharArray(); 
+        char[] ct = t.toCharArray(); 
+
+        Arrays.sort(cs); 
+        Arrays.sort(ct); 
+
+        String ss = new String(cs); 
+        String st = new String(ct); 
+
+        if(ss.equals(st)) return true; 
+
+        return false; 
+    }
+}
