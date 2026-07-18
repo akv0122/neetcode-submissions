0a085c2840e8255a8c34ff50222afb5e776884fe
akv0122 <124851153+akv0122@users.noreply.github.com>
2026-07-18 23:29:27 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyingPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            // profit case
+
+            if (prices[i] > buyingPrice) {
+                maxProfit = Math.max(maxProfit, prices[i] - buyingPrice);
+            } else {
+                buyingPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
